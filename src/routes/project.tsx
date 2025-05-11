f61d57374f2960595de1ee803bf6a9fdd16121ee
jhaywood <jonathan.haywood@concentrix.com>
2025-05-11 13:17:50 -0600

    add projects

--- src/routes/project.tsx
+++ src/routes/project.tsx
@@ -1,9 +1,54 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Particle } from "@/components/Particle";
+import { ProjectCard } from "@/components/ProjectCard";
+import { createFileRoute } from "@tanstack/react-router";
+import { Col, Container, Row } from "react-bootstrap";
+import realTimeAudio from "@/assets/projects/realtime-audio.png";
+import gptAssistant from "@/assets/projects/gpt-assistant.png";
+import gamesAI from "@/assets/projects/games-ai.png";
 
-export const Route = createFileRoute('/project')({
+export const Route = createFileRoute("/project")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/project"!</div>
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Project <strong className="purple">Playground</strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Explore my side projects, small apps and scripts that fuel my passion
+          for code.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={realTimeAudio}
+              title="OpenAI Realtime Audio"
+              description="This project demonstrates how to use OpenAI's new Realtime API beta with audio streaming. When the user speaks, their audio is streamed to Open AI and Open AI's audio response is streamed back to the client."
+              ghLink="https://github.com/JonHaywood/openai-realtime-demo"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={gptAssistant}
+              title="GPT-Assistant"
+              description="GPT-Assistant your own personal Siri, your own 'OK Google', a Typescript, Node.js-based digital home assistant built to run on a Raspberry Pi."
+              ghLink="https://github.com/JonHaywood/gpt-assistant"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={gamesAI}
+              title="Games.AI"
+              description="A C# machine learning library to solve simple games and puzzles. This is a library made (mostly) for fun. It's a C# library that contains machine learning algorithms that solve a handful of different types of games."
+              ghLink="https://github.com/JonHaywood/Games.AI"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 }
